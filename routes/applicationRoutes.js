3ece27f196870f246226e999016bcdeca1cec5fa
faisale <fa6769@gmaile.com>
2026-02-12 20:05:44 +0600

    Add PUT method support for application updates alongside PATCH

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -8,6 +8,7 @@ router.get('/', requireAuth, verifyModerator, applicationController.getAllApplic
 router.get('/user/:email', requireAuth, applicationController.getApplicationsByUser);
 router.get('/:id', requireAuth, applicationController.getApplicationById);
 router.patch('/:id', requireAuth, verifyModerator, applicationController.updateApplicationStatus);
+router.put('/:id', requireAuth, verifyModerator, applicationController.updateApplicationStatus);
 router.delete('/:id', requireAuth, applicationController.deleteApplication);
 
 module.exports = router;
